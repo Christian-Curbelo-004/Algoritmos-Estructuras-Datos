5ac9353733fe83577aa17eb4d4f3f59bd9ba871b
chris <chris94curbelo@gmail.com>
2026-08-22 12:32:10 -0300

    ej sucursales

--- UT01/Ej23/EjSucursales/src/main/java/EjSucursales/App.java
+++ UT01/Ej23/EjSucursales/src/main/java/EjSucursales/App.java
@@ -1,44 +1,23 @@
 package EjSucursales;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
-public class App {
+public class App{
+    public static void main(String[]args) throws FileNotFoundException{
+        DirectorioSucursales directorio;
 
-    public static void main(String[] args) throws FileNotFoundException {
+        directorio = new DirectorioSucursales();
 
-        // Creamos el directorio de sucursales
-        DirectorioSucursales directorio = new DirectorioSucursales();
+        cargarArchivo("sucursales.txt", directorio);
 
-        // Abrimos el archivo ubicado en src/main/resources
-        File archivo = new File("src/main/resources/sucursales.txt");
-
-        // Scanner para leer el archivo
-        Scanner scanner = new Scanner(archivo);
-
-        // Recorremos el archivo línea por línea
-        while (scanner.hasNextLine()) {
-
-            // Cada línea contiene el nombre de una ciudad
-            String ciudad = scanner.nextLine().trim();
-
-            // Agregamos la ciudad a la lista
-            directorio.agregarSucursal(ciudad);
-        }
-
-        // Cerramos el Scanner
-        scanner.close();
-
-        // Mostramos las ciudades cargadas
-        System.out.println("Sucursales:");
-
+        System.out.println("Sucursales");
         directorio.listarSucursales("\n");
 
-        // Mostramos la cantidad total
-        System.out.println(
-            "Cantidad de sucursales: "
-            + directorio.cantidadSucursales()
-        );
+        System.out.println("La cantidad de sucursales" + directorio.cantidadSucursales());
+        
+        directorio = new DirectorioSucursales();
+        cargar 
+
+
     }
 }
